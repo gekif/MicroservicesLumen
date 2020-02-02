f740dd20be7426ad075cda1aa3fa22c5436b9034
Gekif <dzulfikar.maulana@gmail.com>
2020-02-02 09:31:03 +0700

    02. Creating a first microservice for authors with lumen: Lesson 5. Building the authors table with a migration of Lumen Lesson 6. Creating the authors' model Lesson 7. Creating a factory for authors and building test values Lesson 8. Creating the controller for authors Lesson 9. Creating the routes for CRUD operations on the authors Lesson 10. Normalizing the authors' microservice responses

--- LumenAuthorsApi/app/Author.php
+++ LumenAuthorsApi/app/Author.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Author extends Model
 {
     use Authenticatable, Authorizable;
 
@@ -18,15 +18,10 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name',
+        'gender',
+        'country',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+
 }
