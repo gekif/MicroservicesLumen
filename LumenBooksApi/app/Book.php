cf07b6d55feb01841a4884a8971b847b3b123109
Gekif <dzulfikar.maulana@gmail.com>
2020-02-20 09:39:30 +0700

    04. Creating the book microservice with Lumen: Lesson 20. Creating the model for books

--- LumenBooksApi/app/Book.php
+++ LumenBooksApi/app/Book.php
@@ -8,25 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Book extends Model
 {
-    use Authenticatable, Authorizable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'title',
+        'description',
+        'price',
+        'author_id'
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
 }
