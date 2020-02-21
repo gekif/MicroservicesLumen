4f353f6af5c36037317260f15928e156dccbaf5b
Gekif <dzulfikar.maulana@gmail.com>
2020-02-21 21:25:08 +0700

    06. Creating and preparing the API gateway with Lumen: Lesson 33. Creating the controllers for the authors and books Lumen microservices

---
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class BookController
+{
+    use ApiResponser;
+
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    /**
+     * Return the list of books
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+
+    /**
+     * Create one new book
+     * @return Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+
+    /**
+     * Obtains and show one book
+     * @return Illuminate\Http\Response
+     */
+    public function show($book)
+    {
+
+    }
+
+
+    /**
+     * Update existing book
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
+    {
+
+    }
+
+
+    /**
+     * Remove existing book
+     * @return Illuminate\Http\Response
+     */
+    public function destroy($book)
+    {
+
+    }
+
+}
