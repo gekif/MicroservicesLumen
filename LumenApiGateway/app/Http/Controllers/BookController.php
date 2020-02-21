37fec81a646b7f603c97d5346bfc68b61578d2ee
Gekif <dzulfikar.maulana@gmail.com>
2020-02-21 21:31:03 +0700

    06. Creating and preparing the API gateway with Lumen: Lesson 34. Unifying Lumen responses for the API Gateway

--- LumenApiGateway/app/Http/Controllers/BookController.php
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class BookController
--- LumenApiGateway/app/Traits/ApiResponser.php
+++ LumenApiGateway/app/Traits/ApiResponser.php
@@ -10,12 +10,13 @@ trait ApiResponser
      * Build success response
      * @param string|array $data
      * @param int $code
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function successResponse($data, $code = Response::HTTP_OK)
     {
-        return response($data, $code)
-            ->header('Content-Type', 'application/json');
+        return response()->json([
+            'data' => $data
+        ], $code);
     }
 
 
@@ -31,19 +32,6 @@ trait ApiResponser
             'error' => $message,
             'code' => $code
         ], $code);
-    }
-
-
-    /**
-     * Build error message
-     * @param string|array $message
-     * @param int $code
-     * @return \Illuminate\Http\Response
-     */
-    public function errorMessage($message, $code)
-    {
-        return response($message, $code)
-            ->header('Content-Type', 'application/json');
     }
 
 
